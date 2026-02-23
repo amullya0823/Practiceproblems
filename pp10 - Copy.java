f0c1be63d05ec3d4a30d9a20247bb62ae34bc345
amullya0823 <amullyasham@gmail.com>
2026-02-23 11:21:47 +0530

    Add files via upload

---
+++ pp10 - Copy.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class pp10 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        double heightCm;
+        System.out.print("Enter height in cm: ");
+        heightCm = input.nextDouble();
+
+        double inches = heightCm / 2.54;
+        double feet = inches / 12;
+
+        System.out.println("Your Height in cm is " + heightCm +
+                           " while in feet is " + feet +
+                           " and inches is " + inches);
+    }
+}
---
+++ pp2 - Copy.java
@@ -0,0 +1,17 @@
+public class pp2 {
+    public static void main(String[] args) {
+
+        // Marks
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+
+        // Total and average
+        int total = maths + physics + chemistry;
+        double average = total / 3.0;
+
+        // Output
+        System.out.println("Sam’s average mark in PCM is " + average);
+
+    }
+}
