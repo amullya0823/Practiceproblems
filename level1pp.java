99ef3559bbeeb73c57cf532210fea8c6a174ef50
amullya0823 <amullyasham@gmail.com>
2026-02-23 08:59:02 +0530

    Harryage

---
+++ level1pp.java
@@ -0,0 +1,9 @@
+public class level1pp {
+    public static void main(String[] args) {
+        String personName = "Harry";
+        int birthYear = 2000;
+        int currentYear = 2024;
+        int age = currentYear - birthYear;
+        System.out.println(personName + "'s age in " + currentYear + " is " + age);
+    }
+}
