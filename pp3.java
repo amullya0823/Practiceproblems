4aa3c439faadb3c060b94052413d06d5051d7c88
amullya0823 <amullyasham@gmail.com>
2026-02-23 11:25:27 +0530

    Delete pp3.java

--- pp3.java
+++
@@ -1,8 +0,0 @@
-public class pp3 {
-    public static void main(String[] args) {
-        double km = 10.8;
-        double conversionFactor = 1.6;
-        double miles = km / conversionFactor;
-        System.out.println("The distance " + km + " km in miles is " + miles);
-    }
-}
